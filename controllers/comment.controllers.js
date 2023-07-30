701b2e84261a354794216543d0b8ff11e12702c7
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-07-31 01:34:58 +0530

    comments server done

--- controllers/comment.controllers.js
+++ controllers/comment.controllers.js
@@ -71,6 +71,10 @@ const deleteComment = async (req, res) => {
         if (!comment) {
             return res.status(400).json({ msg: "No comment found" })
         }
+        //check if the req.user is the one who creatde the comment
+        if (comment.commentedBy.id.toString() !== req.user._id.toString()) {
+            return res.status(401).json({ msg: "You are not authorized to delete this comment" })
+        }
         await Comment.findByIdAndDelete(commentID);
         res.status(200).json({ msg: "Comment deleted" })
 
